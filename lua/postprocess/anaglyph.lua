8889aeefd474521b24c1f0543dd5f4d8d317bd43
SuperStik <daviswood290@gmail.com>
2021-08-29 14:50:45 -0700

    Redo all drawing functions

--- lua/postprocess/anaglyph.lua
+++ lua/postprocess/anaglyph.lua
@@ -1,23 +1,28 @@
 local width, height = ScrW(), ScrH()
-local tex_left = GetRenderTargetEx("_rt_AnaglyphLeft", width, height, RT_SIZE_NO_CHANGE, MATERIAL_RT_DEPTH_SEPARATE, 258, 0, IMAGE_FORMAT_BGR888)
-local tex_right = GetRenderTargetEx("_rt_AnaglyphRight", width, height, RT_SIZE_NO_CHANGE, MATERIAL_RT_DEPTH_SEPARATE, 258, 0, IMAGE_FORMAT_BGR888)
-local tex_anaglyph = GetRenderTarget("_rt_Anaglyph", width, height, RT_SIZE_NO_CHANGE, MATERIAL_RT_DEPTH_SEPARATE, 258, 0, IMAGE_FORMAT_BGR888)
+local tex_left = GetRenderTargetEx("_rt_AnaglyphLeft", width, height, RT_SIZE_NO_CHANGE, MATERIAL_RT_DEPTH_NONE, 258, 0, IMAGE_FORMAT_BGR888)
+local tex_right = GetRenderTargetEx("_rt_AnaglyphRight", width, height, RT_SIZE_NO_CHANGE, MATERIAL_RT_DEPTH_NONE, 258, 0, IMAGE_FORMAT_BGR888)
+--[[local tex_anaglyph = GetRenderTarget("_rt_Anaglyph", width, height, RT_SIZE_NO_CHANGE, MATERIAL_RT_DEPTH_SEPARATE, 258, 0, IMAGE_FORMAT_BGR888)]]
 local mat_left = CreateMaterial("pp/anaglyphleft", "UnlitGeneric", {
 	["$basetexture"] = "_rt_AnaglyphLeft",
-	["$ignorez"] = "1"
+	--[[["$ignorez"] = "1",
+	["$vertexalpha"] = "1",
+	["$vertexcolor"] = "1"]]
 })
 local mat_right = CreateMaterial("pp/anaglyphright", "UnlitGeneric", {
 	["$basetexture"] = "_rt_AnaglyphRight",
-	["$ignorez"] = "1"
+	["$vertexalpha"] = "1"
+	--[[["$ignorez"] = "1",
+	["$vertexalpha"] = "1",
+	["$vertexcolor"] = "1"]]
 })
-local mat_anaglyph = CreateMaterial("pp/anaglyph", "UnlitGeneric", {
+--[[local mat_anaglyph = CreateMaterial("pp/anaglyph", "UnlitGeneric", {
 	["$basetexture"] = "_rt_Anaglyph",
 	["$ignorez"] = "1"
-})
+})]]
 
 mat_left:SetTexture( "$basetexture", tex_left )
 mat_right:SetTexture( "$basetexture", tex_right )
-mat_anaglyph:SetTexture("$basetexture", tex_anaglyph)
+--mat_anaglyph:SetTexture("$basetexture", tex_anaglyph)
 --[[---------------------------------------------------------
 	Register the convars that will control this effect
 -----------------------------------------------------------]]
@@ -48,7 +53,7 @@ function RenderAnaglyph( ViewOrigin, ViewAngles )
 	render.PushRenderTarget(tex_anaglyph)
 
 	-- Left Draw
-	surface.SetMaterial(mat_left)
+	--[[surface.SetMaterial(mat_left)
 	surface.SetDrawColor(255, 0, 0)
 	surface.DrawTexturedRect(0, 0, w, h)
 	surface.DrawRect(0, 0, 128, 128) -- debugging
@@ -61,7 +66,7 @@ function RenderAnaglyph( ViewOrigin, ViewAngles )
 	render.PopRenderTarget()
 
 	render.SetMaterial(mat_anaglyph)
-	render.DrawScreenQuad()
+	render.DrawScreenQuad()]]
 end
 
 --[[---------------------------------------------------------
@@ -69,7 +74,7 @@ end
 -----------------------------------------------------------]]
 hook.Add( "RenderScene", "RenderAnaglyph", function( ViewOrigin, ViewAngles )
 
-	if ( !pp_anaglyph:GetBool() ) then return end
+	if !pp_anaglyph:GetBool() then return end
 
 	RenderAnaglyph( ViewOrigin, ViewAngles )
 
@@ -77,6 +82,20 @@ hook.Add( "RenderScene", "RenderAnaglyph", function( ViewOrigin, ViewAngles )
 	return true
 
 end )
+
+hook.Add("PreRender", "DrawAnaglyph", function()
+	if !pp_anaglyph:GetBool() then return end
+	cam.Start2D()
+	surface.SetMaterial(mat_left)
+	surface.SetDrawColor(255,255,255)
+	surface.DrawTexturedRect(0,0,1680,1050)
+	surface.SetMaterial(mat_right)
+	--surface.SetDrawColor(255,255,255)
+	surface.SetAlphaMultiplier(0.5)
+	surface.DrawTexturedRect(0,0,1680,1050)
+	surface.SetAlphaMultiplier(1)
+	cam.End2D()
+end)
 
 list.Set( "PostProcess", "Anaglyph", {
 
